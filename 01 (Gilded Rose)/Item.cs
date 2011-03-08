f4189bd46c8e492742a2090d25a12bbe49f42b46
TerryH <terry@tnthughes.net>
2011-03-08 10:28:15 -0800

    Added the first refactoring (Gilded Rose)

---
+++ 01 (Gilded Rose)/Item.cs
@@ -0,0 +1,9 @@
+namespace RefactorThis.GildedRose
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}

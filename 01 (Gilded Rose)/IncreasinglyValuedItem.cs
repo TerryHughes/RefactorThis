8e29d54fd30d2b02d0bebbf3b207461fc48f3466
TerryH <terry@tnthughes.net>
2011-05-02 08:46:53 -0700

    Added IncreasinglyValuedItem class

---
+++ 01 (Gilded Rose)/IncreasinglyValuedItem.cs
@@ -0,0 +1,9 @@
+namespace RefactorThis.GildedRose
+{
+    public class IncreasinglyValuedItem : ValuedItem
+    {
+        public IncreasinglyValuedItem(Item item) : base(item)
+        {
+        }
+    }
+}
--- 01 (Gilded Rose)/ItemFactory.cs
+++ 01 (Gilded Rose)/ItemFactory.cs
@@ -9,9 +9,14 @@ namespace RefactorThis.GildedRose
                 return new LegendaryItem(item);
             }
 
-            if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name == "Aged Brie")
             {
                 return new ValuedItem(item);
+            }
+
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                return new IncreasinglyValuedItem(item);
             }
 
             return new ExtendedItem(item);

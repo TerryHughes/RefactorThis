8d2d3e52e7166325f7cf92942840e6670f181e44
TerryH <terry@tnthughes.net>
2011-05-02 08:54:01 -0700

    Removed redundant override on ValuedItem

--- 01 (Gilded Rose)/ValuedItem.cs
+++ 01 (Gilded Rose)/ValuedItem.cs
@@ -13,18 +13,5 @@ namespace RefactorThis.GildedRose
                 this.Item.Quality = this.Item.Quality + 1;
             }
         }
-
-        public override void UpdateSellIn()
-        {
-            this.Item.SellIn = this.Item.SellIn - 1;
-
-            if (this.Item.SellIn < 0)
-            {
-                if (this.Item.Quality < 50)
-                {
-                    this.Item.Quality = this.Item.Quality + 1;
-                }
-            }
-        }
     }
 }

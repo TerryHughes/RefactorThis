3221127cf2c7d32f0e2a950072357a4a7178f5d4
TerryH <terry@tnthughes.net>
2011-05-02 06:45:41 -0700

    Moved calculation code into ExtendedItem class

--- 01 (Gilded Rose)/ExtendedItem.cs
+++ 01 (Gilded Rose)/ExtendedItem.cs
@@ -11,10 +11,83 @@ namespace RefactorThis.GildedRose
 
         public void UpdateQuality()
         {
+            if (this.Item.Name != "Aged Brie" && this.Item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (this.Item.Quality > 0)
+                {
+                    if (this.Item.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        this.Item.Quality = this.Item.Quality - 1;
+
+                        if (this.Item.Name == "Conjured Mana Cake")
+                        {
+                            this.Item.Quality = this.Item.Quality - 1;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (this.Item.Quality < 50)
+                {
+                    this.Item.Quality = this.Item.Quality + 1;
+
+                    if (this.Item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.Item.SellIn < 11)
+                        {
+                            if (this.Item.Quality < 50)
+                            {
+                                this.Item.Quality = this.Item.Quality + 1;
+                            }
+                        }
+
+                        if (this.Item.SellIn < 6)
+                        {
+                            if (this.Item.Quality < 50)
+                            {
+                                this.Item.Quality = this.Item.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         public void UpdateSellIn()
         {
+            if (this.Item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                this.Item.SellIn = this.Item.SellIn - 1;
+            }
+
+            if (this.Item.SellIn < 0)
+            {
+                if (this.Item.Name != "Aged Brie")
+                {
+                    if (this.Item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.Item.Quality > 0)
+                        {
+                            if (this.Item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                this.Item.Quality = this.Item.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this.Item.Quality = this.Item.Quality - this.Item.Quality;
+                    }
+                }
+                else
+                {
+                    if (this.Item.Quality < 50)
+                    {
+                        this.Item.Quality = this.Item.Quality + 1;
+                    }
+                }
+            }
         }
     }
 }
--- 01 (Gilded Rose)/Program.cs
+++ 01 (Gilded Rose)/Program.cs
@@ -2,6 +2,7 @@ namespace RefactorThis.GildedRose
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     internal class Program
     {
@@ -65,82 +66,10 @@ namespace RefactorThis.GildedRose
 
         private static void UpdateQuality()
         {
-            foreach (var item in Items)
+            foreach (var item in Items.Select(i => new ExtendedItem(i)))
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality = item.Quality - 1;
-
-                            if (item.Name == "Conjured Mana Cake")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
+                item.UpdateQuality();
+                item.UpdateSellIn();
             }
         }
 

77f3a71b46a2a55fdff92f3d9712063db073bbc9
TerryH <terry@tnthughes.net>
2011-05-02 08:43:38 -0700

    Isolated valued specific logic

--- 01 (Gilded Rose)/ExtendedItem.cs
+++ 01 (Gilded Rose)/ExtendedItem.cs
@@ -11,42 +11,13 @@ namespace RefactorThis.GildedRose
 
         public virtual void UpdateQuality()
         {
-            if (this.Item.Name != "Aged Brie" && this.Item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (this.Item.Quality > 0)
             {
-                if (this.Item.Quality > 0)
+                this.Item.Quality = this.Item.Quality - 1;
+
+                if (this.Item.Name == "Conjured Mana Cake")
                 {
                     this.Item.Quality = this.Item.Quality - 1;
-
-                    if (this.Item.Name == "Conjured Mana Cake")
-                    {
-                        this.Item.Quality = this.Item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (this.Item.Quality < 50)
-                {
-                    this.Item.Quality = this.Item.Quality + 1;
-
-                    if (this.Item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (this.Item.SellIn < 11)
-                        {
-                            if (this.Item.Quality < 50)
-                            {
-                                this.Item.Quality = this.Item.Quality + 1;
-                            }
-                        }
-
-                        if (this.Item.SellIn < 6)
-                        {
-                            if (this.Item.Quality < 50)
-                            {
-                                this.Item.Quality = this.Item.Quality + 1;
-                            }
-                        }
-                    }
                 }
             }
         }
@@ -57,26 +28,9 @@ namespace RefactorThis.GildedRose
 
             if (this.Item.SellIn < 0)
             {
-                if (this.Item.Name != "Aged Brie")
+                if (this.Item.Quality > 0)
                 {
-                    if (this.Item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (this.Item.Quality > 0)
-                        {
-                            this.Item.Quality = this.Item.Quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        this.Item.Quality = this.Item.Quality - this.Item.Quality;
-                    }
-                }
-                else
-                {
-                    if (this.Item.Quality < 50)
-                    {
-                        this.Item.Quality = this.Item.Quality + 1;
-                    }
+                    this.Item.Quality = this.Item.Quality - 1;
                 }
             }
         }
--- 01 (Gilded Rose)/ValuedItem.cs
+++ 01 (Gilded Rose)/ValuedItem.cs
@@ -5,5 +5,53 @@ namespace RefactorThis.GildedRose
         public ValuedItem(Item item) : base(item)
         {
         }
+
+        public override void UpdateQuality()
+        {
+            if (this.Item.Quality < 50)
+            {
+                this.Item.Quality = this.Item.Quality + 1;
+
+                if (this.Item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (this.Item.SellIn < 11)
+                    {
+                        if (this.Item.Quality < 50)
+                        {
+                            this.Item.Quality = this.Item.Quality + 1;
+                        }
+                    }
+
+                    if (this.Item.SellIn < 6)
+                    {
+                        if (this.Item.Quality < 50)
+                        {
+                            this.Item.Quality = this.Item.Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void UpdateSellIn()
+        {
+            this.Item.SellIn = this.Item.SellIn - 1;
+
+            if (this.Item.SellIn < 0)
+            {
+                if (this.Item.Name == "Aged Brie")
+                {
+                    if (this.Item.Quality < 50)
+                    {
+                        this.Item.Quality = this.Item.Quality + 1;
+                    }
+                }
+
+                if (this.Item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    this.Item.Quality = this.Item.Quality - this.Item.Quality;
+                }
+            }
+        }
     }
 }
